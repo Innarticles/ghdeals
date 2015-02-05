630173686c03787249538c5a2684c90ba164fc4d
Innocent <innocentudeh@gmail.com>
2015-02-05 16:40:58 +0000

    added one div display to list page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'promos/list' => 'promos#list'
+
   resources :promos
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
